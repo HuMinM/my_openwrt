fc77896750f8d439e94c8dff3eccb3b88f5800b7
HuMinM <270510459@qq.com>
2021-01-13 14:02:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,13 +12,6 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
-echo "src-git jerryk https://github.com/jerrykuku/openwrt-package.git" >> ./feeds.conf.default
-echo "src-git openclash https://github.com/vernesong/OpenClash;master" >> ./feeds.conf.default
-rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
-mkdir -p package/lixunhuan
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lixunhuan/luci-app-jd-dailybonus
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lixunhuan/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lixunhuan/luci-app-vssr
+
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
