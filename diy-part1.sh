6764c9c937fa95a6bb37ca55ae682ac745b711a9
HuMinM <270510459@qq.com>
2021-01-28 01:36:24 -0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,6 @@
 
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
 mkdir -p package/lixunhuan
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lixunhuan/luci-app-jd-dailybonus
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lixunhuan/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lixunhuan/luci-app-argon-config
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lixunhuan/luci-app-vssr
